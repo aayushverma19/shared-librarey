d4fe5bfcfa92a4785453d5aa6be2eb005a453e0f
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-04 14:06:43 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,8 +1,8 @@
  import org.example.my
 
 def call(String name = 'World') {
-    def message = my.getHelloMessage(name)
-   // def message = Utils.getHelloMessage(name)
+    //def message = my.getHelloMessage(name)
+   /def message = Utils.getHelloMessage(name)
     echo message
 
        // echo "hello ${name}"  // only use vars file and comment import org.example.Utils, def message = Utils.getHelloMessage(name)
