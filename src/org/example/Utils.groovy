45e105249bb1fc4d4a6967d7c6edb0a061f5073a
aayushverma19 <140351437+aayushverma19@users.noreply.github.com>
2025-03-04 12:28:11 +0530

    Update and rename Utils.groovy to Utils.groovy

---
+++ src/org/example/Utils.groovy
@@ -0,0 +1,7 @@
+package com.example
+
+class Utils {
+    static String getHelloMessage(String name) {
+        return "Hello, ${name}!"
+    }
+}
--- src/org/mycompany/Utils.groovy
+++
@@ -1,7 +0,0 @@
-package org.mycompany
-
-class Utils {
-    static String greet(String name) {
-        return "Hello, ${name}! Welcome to Jenkins Shared Library."
-    }
-}
